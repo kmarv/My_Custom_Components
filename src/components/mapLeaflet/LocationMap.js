480aaf6842d4fe2c9d475a47a52b89a4274d4685
kmarv <marvkaay@gmail.com>
2023-10-12 12:38:24 +0300

    adding current location to the map

--- src/components/mapLeaflet/LocationMap.js
+++ src/components/mapLeaflet/LocationMap.js
@@ -1,30 +1,36 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import {
   MapContainer,
   TileLayer,
   Marker,
   Popup,
-  useMapEvents,
+  useMap,
 } from "react-leaflet";
 
 function LocationMarker() {
   const [position, setPosition] = useState(null);
-  const map = useMapEvents({
-    click() {
-      map.locate();
-    },
-    locationfound(e) {
-      setPosition(e.latlng);
-      map.flyTo(e.latlng, map.getZoom());
-    },
-  });
+  const map = useMap();
   const markerRef = useRef(null);
 
   const handleMarkerDragEnd = (e) => {
     const marker = e.target;
     setPosition(marker.getLatLng());
   };
-  console.log(position);
+
+  useEffect(() => {
+    // Fetch user's current location using the geolocation API
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition((location) => {
+        const { latitude, longitude } = location.coords;
+        const userPosition = [latitude, longitude];
+        setPosition(userPosition);
+        map.flyTo(userPosition, map.getZoom());
+        if (markerRef.current) {
+          markerRef.current.setLatLng(userPosition);
+        }
+      });
+    }
+  }, [map]);
 
   return position === null ? null : (
     <Marker
@@ -39,19 +45,20 @@ function LocationMarker() {
     </Marker>
   );
 }
+
 const LocationMap = () => {
   return (
-      <MapContainer
-        center={{ lat: 51.505, lng: -0.09 }}
-        zoom={153}
-        style={{ position: "absolute", top: 0, left: 0, right: 0, bottom: 0, height:'400px', width:'100%' }}
-      >
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        />
-        <LocationMarker />
-      </MapContainer>
+    <MapContainer
+    center={{lat:51.505, lng:-0.09}}
+    zoom={153}
+      style={{ position: "absolute", top: 0, left: 0, right: 0, bottom: 0, height: '800px', width: '100%' }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <LocationMarker />
+    </MapContainer>
   );
 };
 
