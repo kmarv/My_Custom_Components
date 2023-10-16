cfe2c5d721e7121805208333b9fe4e8611c5f3b4
kmarv <marvkaay@gmail.com>
2023-10-16 13:19:29 +0300

    adding map location pin drag on google maps

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 // import LocationMap from './components/mapLeaflet/LocationMap';
-import MapWithDraggablePin from './components/mapLeaflet/Map';
+import LocationSelector from './components/mapLeaflet/Map';
 
 function App() {
   return (
     <div className="row">
       <div className='col-md-12'>
-      <MapWithDraggablePin  />
+      <LocationSelector zoom={19} initialLat={51.505} initialLng={-0.09}  locationName={'london'}  />
       </div>
     </div>
   );
--- src/components/mapLeaflet/Map.js
+++ src/components/mapLeaflet/Map.js
@@ -1,38 +1,54 @@
-import React, { useState, useEffect } from 'react';
-import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
+import React, { useState, useEffect } from "react";
+import { GoogleMap, LoadScript, Marker, Circle } from "@react-google-maps/api";
+import PropTypes from "prop-types";
 
-const LocationSelector = (props) => {
-    
-  const [location, setLocation] = useState(); // Initialize with default coordinates
-
-  const handleMarkerDrag = (coord, map, e) => {
-    setLocation({ lat: coord.lat(), lng: coord.lng() });
+const LocationSelector = ({ initialLat, initialLng, zoom }) => {
+  const [location, setLocation] = useState({
+    lat: initialLat,
+    lng: initialLng,
+  });
+  const [radius, setRadius] = useState(500); // Initial radius in meters
+  const handleMarkerDrag = (e) => {
+    const newLocation = { lat: e.latLng.lat(), lng: e.latLng.lng() };
+    setLocation(newLocation);
   };
 
-  useEffect(() => {
-    // Fetch user's current location using the geolocation API
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition((location) => {
-        const { latitude, longitude } = location.coords;
-        const userPosition = {latitude, longitude};
-        setLocation(userPosition);
-      });
-    }
-  }, [location]);
+  console.log(location); 
 
   return (
-    <Map google={props.google} initialCenter={location}>
-      <Marker
-        name={'Location Pin'}
-        position={location}
-        draggable={true}
-        onDrag={handleMarkerDrag}
-        onDragend={handleMarkerDrag}
-      />
-    </Map>
+    <>
+      <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_KEY}>
+        <GoogleMap
+          center={location}
+          zoom={zoom}
+          mapContainerStyle={{ width: "100%", height: "800px" }}
+        >
+          <Marker
+            position={location}
+            draggable={true}
+            onDragEnd={handleMarkerDrag}
+            key={Math.random()}
+          />
+          <Circle
+            center={location}
+            radius={radius}
+            options={{
+              strokeColor: "#FF0000",
+              strokeOpacity: 0.8,
+              strokeWeight: 2,
+              fillColor: "#FF0000",
+              fillOpacity: 0.35,
+            }}
+          />
+        </GoogleMap>
+      </LoadScript>
+    </>
   );
 };
-
-export default GoogleApiWrapper({
-  apiKey: process.env.REACT_APP_GOOGLE_MAPS_KEY,
-})(LocationSelector);
+LocationSelector.propTypes = {
+  initialLat: PropTypes.number,
+  initialLng: PropTypes.number,
+  locationName: PropTypes.string,
+  zoom: PropTypes.number,
+};
+export default LocationSelector;
